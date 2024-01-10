11f4bb835f032087b9329546bf0c0e4c3682c91f
VinitVora <68478128+VinitVora@users.noreply.github.com>
2024-01-10 14:37:26 +0530

    script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = ""; // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
